b6af1202c6a4a2f1e4de4e25ac106eee4d54e3cc
Narendra <nkadiri09@gmail.com>
2022-07-16 17:24:46 -0400

    Added Spring discovery

--- src/test/java/com/naren/career/ratingdataservice/RatingDataServiceApplicationTests.java
+++ src/test/java/com/naren/career/ratingdataservice/RatingDataServiceApplicationTests.java
@@ -1,12 +1,11 @@
 package com.naren.career.ratingdataservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RatingDataServiceApplicationTests {
 
-    @Test
+    //@Test
     void contextLoads() {
     }
 
